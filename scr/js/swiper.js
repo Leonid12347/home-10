3ad268f6b13f2bd7af817dc72d64a763b2943bdb
Leonid12347 <124260342+Leonid12347@users.noreply.github.com>
2023-02-20 16:21:35 +0300

    first commit

---
+++ scr/js/swiper.js
@@ -0,0 +1,20 @@
+
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+    
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+export default swiper;
